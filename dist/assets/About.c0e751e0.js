b3ec13c5133537514266f17224161fb19bc2da7d
regxxxman <regxxxman@gmail.com>
2023-02-12 16:44:48 +0300

    Adding dist

---
+++ dist/assets/About.c0e751e0.js
@@ -0,0 +1,1 @@
+import{_ as a,o as s,c as _,a as o,b as c,F as p,p as r,d,e as t}from"./index.3bbc78fc.js";const n=e=>(r("data-v-67b65f6e"),e=e(),d(),e),u=n(()=>t("div",{class:"bg min-h-screen text-white"},[t("h2",{class:"pt-20"},"About")],-1)),l={setup(e){return(i,b)=>(s(),_(p,null,[o(c),u],64))}};var h=a(l,[["__scopeId","data-v-67b65f6e"]]);export{h as default};
